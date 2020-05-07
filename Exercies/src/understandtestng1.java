ef916d92def95dba517725904a0cc49d7305936f
arachha <aparnareddyrachha@gmail.com>
2020-05-07 16:54:59 -0400

    Code pushed

---
+++ Exercies/src/understandtestng1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class understandtestng1 {
+
+	@Test
+	public void testng() {
+		// TODO Auto-generated method stub
+		System.out.println("Understand testng.xml file");
+		
+		List l = new ArrayList();
+	}
+
+}
+
+
